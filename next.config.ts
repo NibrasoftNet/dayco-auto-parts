ccce57ff1c25c5e0d15291aefea509af84ccecb5
Personal Dev <dev.gen.info.0@gmail.com>
2025-08-28 14:05:26 +0100

    feat(images): add remote patterns to next js config images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "auto-car-parts.s3.us-east-1.amazonaws.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
